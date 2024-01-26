155e4a300f91e5ce13c021595211d9499696c925
elvis-visi <sivisevi1@gmail.com>
2024-01-26 10:12:10 +0100

    Added logging and unkownEndpoint middleware

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+app.use(requestLogger);
+
 let phonebook = [
   {
     id: 1,
@@ -87,6 +96,12 @@ app.post("/api/persons/", (request, response) => {
 
   response.json(per);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3002;
 app.listen(PORT, () => {
