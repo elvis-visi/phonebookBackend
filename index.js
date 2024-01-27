2475b268cc032951b9f8708044696a59cbd67cb1
elvis-visi <sivisevi1@gmail.com>
2024-01-27 13:03:43 +0100

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 app.use(express.json());
-
+app.use(cors());
 app.use(morgan("tiny"));
 
 let phonebook = [
@@ -97,7 +98,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3002;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
