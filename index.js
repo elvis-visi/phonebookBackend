bc3aaea0bcca7f554cfe12661e22a449c8a1a9e5
elvis-visi <sivisevi1@gmail.com>
2024-01-28 19:04:13 +0100

    Added mongoose validation

--- index.js
+++ index.js
@@ -54,24 +54,21 @@ app.delete("/api/persons/:id", (request, response) => {
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, {new : true})
+  Person.findByIdAndUpdate(
+    request.params.id,
+     {name,number},
+      {new: true, runValidators: true, context: 'query'})
   .then(upatedPerson => {
     response.json(upatedPerson)
   })
   .catch(error => next(error))
 
-
 })
 
 
-app.post("/api/persons/", (request, response) => {
+app.post("/api/persons/", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -88,7 +85,7 @@ app.post("/api/persons/", (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
-
+  .catch(error => next(error))
 });
 
 const unknownEndpoint = (request, response) => {
@@ -102,9 +99,11 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-  next(error)
+  next(error) 
 }
 
 // this has to be the last loaded middleware.
